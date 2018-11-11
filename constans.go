5447081c374082ed381b6ca05c7c283f59f3b4cd
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-11 17:21:36 +0900

    同名の関数は認められない?

---
+++ constans.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+const Pi = 3.14
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
+func sub () {
+	fmt.Println(Pi, Username, Password)
+}
--- variable.go
+++ variable.go
@@ -12,9 +12,11 @@ func main () {
 	fmt.Println(i, f64, s, t, f)
 
 	xi := 1
+	xi = 2
 	xf64 := 1.2
 	xs := "test"
 	xt, xf := true, false
 	fmt.Println(xi, xf64, xs, xt, xf)
-	fmt.Printf("%T", xf64)
+	fmt.Printf("%T\n", xf64)
+	fmt.Printf("%T\n", xi)
 }
