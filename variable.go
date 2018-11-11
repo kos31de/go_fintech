01a8b4c998607596a3e59f3db3d6f532cab203b6
kou_sy <e153416@eve.u-ryukyu.ac.jp>
2018-11-11 16:51:11 +0900

    :=は関数内でしか使えない

--- variable.go
+++ variable.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func main () {
-	var i int = 1
-	var f64 float64 = 1.2
-	var s string = "test"
-	var t, f bool = true, false
-
+	var ( i int = 1
+		 f64 float64 = 1.2
+		 s string = "test"
+		 t, f bool = true, false
+	)
 	fmt.Println(i, f64, s, t, f)
 
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
 }
